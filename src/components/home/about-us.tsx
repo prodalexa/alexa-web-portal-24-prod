5a1e39150ff57cb0dc09dd61706c6608d0ea65ab
bhumika1331 <sharmabhumika54@gmail.com>
2024-08-13 00:33:01 +0530

    changed let to const

--- src/components/home/about-us.tsx
+++ src/components/home/about-us.tsx
@@ -3,8 +3,8 @@ import Image from "next/image";
 import { TextGenerateEffect } from "../ui/text-generate-effect";
 
 export default function AboutUs() {
-  let para1 = "Welcome to Alexa Developers SRM, a thriving, student-run technical organization at SRMIST that brings technology and community together. We create an open atmosphere for tech enthusiasts to explore, learn, and grow, with a focus on voice-controlled technologies, application development, and UI/UX design via hands-on events and workshops. Our club is a hotbed of collaboration and innovation, where members can experiment, share expertise, and make a real difference. Whether you're a newcomer to technology or a seasoned professional, our wide range of workshops, hackathons, and contests provide ongoing learning and practical skill development.";
-  let para4 = "Together we build a community, where creativity knows no bounds and collaboration is key.";
+  const para1 = "Welcome to Alexa Developers SRM, a thriving, student-run technical organization at SRMIST that brings technology and community together. We create an open atmosphere for tech enthusiasts to explore, learn, and grow, with a focus on voice-controlled technologies, application development, and UI/UX design via hands-on events and workshops. Our club is a hotbed of collaboration and innovation, where members can experiment, share expertise, and make a real difference. Whether you're a newcomer to technology or a seasoned professional, our wide range of workshops, hackathons, and contests provide ongoing learning and practical skill development.";
+  const para4 = "Together we build a community, where creativity knows no bounds and collaboration is key.";
 
   return (
     <div className="h-full w-auto flex flex-col items-center justify-center bg-transparent antialiased bg-grid-white/[0.02] relative overflow-hidden pb-8">
