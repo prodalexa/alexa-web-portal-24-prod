5ca79ee47ec84aef64ff850ceceef297f43d7419
Harsh Banka <harshbanka321@gmail.com>
2024-08-19 00:54:59 +0530

    feat: add alexaverse favicon

--- src/app/(flagship)/alexaverse/layout.tsx
+++ src/app/(flagship)/alexaverse/layout.tsx
@@ -1,5 +1,4 @@
 import type { Metadata } from "next";
-// import { Inter as FontSans } from "next/font/google"
 import { Nunito as FontSans } from "next/font/google";
 import { SpeedInsights } from "@vercel/speed-insights/next";
 import { Analytics } from "@vercel/analytics/react";
@@ -7,7 +6,6 @@ import { cn } from "@/lib/utils";
 import "./globals.css";
 import Header from "@/components/alexaverse/header";
 import localFont from "next/font/local";
-import ContactUs from "@/components/global/contact-us";
 import { ThemeProvider } from "@/components/alexaverse/theme-providor";
 import { Toaster } from "@/components/ui/toaster";
 
@@ -22,7 +20,7 @@ export const metadata: Metadata = {
   title: "AlexaVerse | ADS!",
   description: "Created with ♥️ by the ADS team.",
   icons: {
-    icon: "/icon.png",
+    icon: "/icon.jpeg",
   },
 };
 
