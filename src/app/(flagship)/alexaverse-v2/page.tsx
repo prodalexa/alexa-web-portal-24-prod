0c8485b53ff4a03fa306be80afab62d522110022
Nikunj Mathur <nikunjmathur0810@gmail.com>
2025-08-12 00:07:22 +0530

    feat: remove sponsor section

--- src/app/(flagship)/alexaverse-v2/page.tsx
+++ src/app/(flagship)/alexaverse-v2/page.tsx
@@ -2,7 +2,7 @@
 
 import HeroSection from "@/components/alexaverse-v2/HeroSection";
 import OurEvents from "@/components/alexaverse-v2/OurEvents";
-import Sponsers from "@/components/alexaverse-v2/Sponsers"
+// import Sponsers from "@/components/alexaverse-v2/Sponsers"
 import ContactUs from "@/components/alexaverse-v2/ContactUs"
 
 export default function AlexaVersePage() {
@@ -11,7 +11,7 @@ export default function AlexaVersePage() {
       <HeroSection />
       <div className="bg-gradient-to-br from-[#030645] via-[#1A052A] to-[#511e5b]">
         <OurEvents />
-        <Sponsers />
+        {/* <Sponsers /> */}
         <ContactUs />
       </div>
     </main>
