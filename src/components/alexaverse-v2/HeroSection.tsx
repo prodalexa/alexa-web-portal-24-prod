2bbe1ad8cbee37d874e3b72b7beada1832be0504
Nikunj Mathur <nikunjmathur0810@gmail.com>
2025-08-29 12:04:14 +0530

    feat: close vlogit hero section

--- src/components/alexaverse-v2/HeroSection.tsx
+++ src/components/alexaverse-v2/HeroSection.tsx
@@ -37,7 +37,7 @@ const HeroSection: React.FC = () => {
         router.push("https://forms.gle/P8PdoBQvTJoNdK51A");
         break;
       case "Vlogit":
-        router.push("/alexaverse-v2/RegisterVlogit");
+        openModal(`Registation for ${eventName} are now closed!`, `${eventName}`)
         break;
       case "Debug the Campus":
         router.push("/alexaverse-v2/RegisterDebug");
