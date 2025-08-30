f9e915b93cc00eefde5ef4428f340afe29329c82
Pratham-pal <prattham.pal22@gmail.com>
2025-08-31 01:18:38 +0530

    sponsor section added

--- sanity.config.ts
+++ sanity.config.ts
@@ -1,24 +1,29 @@
-import { visionTool } from "@sanity/vision";
-import { defineConfig } from "sanity";
-import { structureTool } from "sanity/structure";
-import schemaTypes from "@/sanity/schemas";
+// import { visionTool } from "@sanity/vision";
+// import { defineConfig } from "sanity";
+// import { structureTool } from "sanity/structure";
+// import schemaTypes from "@/sanity/schemas";
 
-const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!
-const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET!
-const apiVersion = process.env.NEXT_PUBLIC_SANITY_API_VERSION || '2023-05-03'
+// const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!
+// const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET!
+// const apiVersion = process.env.NEXT_PUBLIC_SANITY_API_VERSION || '2023-05-03'
 
-const config = defineConfig({
-  name: "default",
-  title: "alexa-web-portal-24",
-  projectId,
-  dataset,
-  apiVersion,
+// const config = defineConfig({
+//   name: "default",
+//   title: "alexa-web-portal-24",
+//   projectId,
+//   dataset,
+//   apiVersion,
 
-  plugins: [structureTool(), visionTool()],
+//   plugins: [structureTool(), visionTool()],
 
-  schema: {
-    types: schemaTypes,
-  },
-});
+//   schema: {
+//     types: schemaTypes,
+//   },
+// });
 
-export default config;
+// export default config;
+export const client = {
+  fetch: async () => {
+    return []; // return empty mock data for now
+  }
+};
