fd6cffac1cfd987d65351aa930a553856df72127
Shubhankar Sarkar <shubhankar.rony@gmail.com>
2025-09-18 18:22:02 +0530

    Update page.tsx

--- src/app/(recruitment)/recruitment25registerform/page.tsx
+++ src/app/(recruitment)/recruitment25registerform/page.tsx
@@ -3,7 +3,7 @@ import ContactUs from "@/components/recruitments25/ContactUs";
 
 export default function Recruitment25RegisterPage() {
   return (
-    <main className="min-h-screen flex items-center justify-center bg-[url('/recruitments25/RegisterFormBG.svg')] bg-cover bg-center">
+    <main className="min-h-screen flex flex-col justify-center bg-[url('/recruitments25/RegisterFormBG.svg')] bg-cover bg-center">
       <RegisterForm />
       <ContactUs />
     </main>
